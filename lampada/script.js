82e85a81bf3f6e536b85f1a4e7101748daea431d
KUWETEC <kawe.coelhofilho@gmail.com>
2022-06-23 16:43:55 -0300

    adicionado os eventos javascript

--- lampada/script.js
+++ lampada/script.js
@@ -4,3 +4,17 @@ const turnOff = document.getElementById('turnOff');
 console.log(lamp);
 console.log(turnOn);
 console.log(turnOff);
+
+function lampiao(){
+    lamp.src = './image/ligada.jpg'
+}
+
+function lampioff(){
+    lamp.src = './image/desligada.jpg'
+}
+
+
+// evento em javascritp
+turnOn.addEventListener('click', lampiao)
+
+turnOff.addEventListener('click', lampioff)
