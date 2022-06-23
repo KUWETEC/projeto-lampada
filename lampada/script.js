da4f63a25a5f639fbbc1144d45d0f9f8c267b8c4
KUWETEC <kawe.coelhofilho@gmail.com>
2022-06-23 16:25:54 -0300

    construção do arquivo js e configurando

---
+++ lampada/script.js
@@ -0,0 +1,6 @@
+const lamp = document.getElementById('lamp');
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+console.log(lamp);
+console.log(turnOn);
+console.log(turnOff);
