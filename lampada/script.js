b01790890129f39360e8118091339fea0bbdb0d8
KUWETEC <kawe.coelhofilho@gmail.com>
2022-06-28 15:23:47 -0300

    function adicionada no script.js

--- lampada/script.js
+++ lampada/script.js
@@ -5,16 +5,36 @@ console.log(lamp);
 console.log(turnOn);
 console.log(turnOff);
 
+function lampadaQuebrada(){
+    return lamp.src.indexOf( 'quebrada' ) > -1
+}
+
 function lampiao(){
-    lamp.src = './image/ligada.jpg'
+    if ( !lampadaQuebrada () ){
+        lamp.src = './image/ligada.jpg'
+        lamp.alt = 'Lampada Ligada'
+        turnOn.innerHTML = 'Desligada';
+    }
 }
 
-function lampioff(){
-    lamp.src = './image/desligada.jpg'
+function lampoff(){
+    if ( !lampadaQuebrada()){
+        lamp.src = './image/desligada.jpg'
+        lamp.alt = 'Lâmpada Desligada'
+    }
 }
 
+function breakLamp(){
+    lamp.src = './image/quebrada.jpg'
+}
 
 // evento em javascritp
 turnOn.addEventListener('click', lampiao)
 
-turnOff.addEventListener('click', lampioff)
+turnOff.addEventListener('click', lampoff)
+
+lamp.addEventListener('mouseover', lampiao)
+
+lamp.addEventListener('mouseleave', lampoff)
+
+lamp.addEventListener('dblclick', breakLamp)
